531f74e4d8c1509d880379d21edbc6095f90f57e
DM <deanmkv@gmail.com>
2015-12-08 13:06:48 -0500

    starting lan notes

--- lan.py
+++ lan.py
@@ -1,14 +1,19 @@
 import socket
+import bitstring
 
 
-# port = 5000
-# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# s.bind(("", port))
-# print "waiting on port:", port
-# while 1:
-#     data, addr = s.recvfrom(1024)
-#     print data, addr
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("localhost", 5000))
 
 
-import struct
+import random
+num = int( random.random() * 4000 + 1)
+print num
 
+
+# to_send = (frame_fmt, 16, b'\x00\x00', b'\x01', b'\x01', b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01', num )
+s.send(to_send)
+
+
+
+s.close()
