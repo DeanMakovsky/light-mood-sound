f80951c6490228e6e9614b601dfc5bbd9a43591f
nathan-williams <nlw2sx@virginia.edu>
2015-12-08 20:32:54 -0500

    Hex to Binary

--- bitstring_work_nate.py
+++ bitstring_work_nate.py
@@ -36,7 +36,29 @@ temp = protocol + source \
 
 size = BitArray(uintle=len(temp + 16)/8, length = 16)
 
-toSend = size + temp
+hexToBinary = {
+	'0':'0000',
+	'1':'0001',
+	'2':'0010',
+	'3':'0011',
+	'4':'0100',
+	'5':'0101',
+	'6':'0110',
+	'7':'0111',
+	'8':'1000',
+	'9':'1001',
+	'a':'1010',
+	'b':'1011',
+	'c':'1100',
+	'd':'1101',
+	'e':'1110',
+	'f':'1111',
+}
+
+foo = str(size + temp).replace('0x','')
+toSend = ''.join([hexToBinary[c] for c in foo])
+
+print toSend
 
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
