2d3d77a839b55393f0c4873633f2e21a89868581
Ethan Pilot <egsadmin@gmail.com>
2015-12-12 10:42:46 -0500

    fix silly mistake

--- colorpickserver.py
+++ colorpickserver.py
@@ -4,19 +4,10 @@ import os
 import lifx
 
 urls = (
-    "172.27.99.15:80/color", "colorme",
+    "/color", "colorme",
 )
 
 
-class dbread:
-    def GET(self):
-        data = db.select('SensorData', order='Time DESC', limit=40)
-
-        #TODO: read the sensor data and render the corresponding website with the input data table
-        data = [[(d['Time'] - datetime(1970,1,1)).total_seconds(),d['Temperature'],d['Humidity']] for d in list(data)]
-        # data = [[str(d['Time']),d['Temperature'],d['Humidity']] for d in list(data)]
-        data.sort()
-        return render.wg(data)
 class colorme:
     def GET(self):
         data=web.input();
@@ -30,3 +21,6 @@ def run():
 
     app = web.application(urls, globals())
     app.run()
+
+if __name__=="__main__":
+    run()
